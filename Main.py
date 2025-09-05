00c14e4196f52abe6ba39973e60bfafba8299189
JonatanVG <jonatangr@bfk.no>
2025-09-05 13:09:31 +0200

    Version 2.11 !Ping fix

--- Main.py
+++ Main.py
@@ -392,7 +392,7 @@ async def shutdown(ctx, name: str = ""):
 async def ping(ctx, name: str = ""):
     if bot.user.name == name:
         latency = round(bot.latency * 1000) # Converts the latency to ms.
-        embed = discord.Embed(leading="Pong!", description=f"Latency: {latency}ms", color=discord.Color.blurple())
+        embed = discord.Embed(title="Pong!", description=f"Latency: {latency}ms", color=discord.Color.blurple())
         await ctx.send(embed=embed, ephemeral=True) # Sends the embed to the user who executed the command.
     else:
         None
